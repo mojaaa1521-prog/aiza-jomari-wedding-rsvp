5d063f7f44d4c8406518e7ed6b13d85333ad2587
mojaaa1521-prog <jcvaldez5@up.edu.ph>
2026-09-03 13:55:01 +0800

    Add wedding link preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,37 @@ const body = Montserrat({
 });
 
 export const metadata: Metadata = {
-  title: "Aiza & Jomari",
-  description: "Wedding Invitation",
+  title: "Aiza & Jomari | Wedding Invitation",
+  description:
+    "Join Aiza & Jomari as they celebrate their wedding on December 17, 2026.",
+
+  openGraph: {
+    title: "Aiza & Jomari | Wedding Invitation",
+    description:
+      "Join us as we celebrate our special day on December 17, 2026.",
+    url: "https://aiza-jomari-wedding-rsvp.vercel.app/",
+    siteName: "Aiza & Jomari Wedding",
+    images: [
+      {
+        url: "https://aiza-jomari-wedding-rsvp.vercel.app/couple/hero.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Aiza & Jomari Wedding Invitation",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Aiza & Jomari | Wedding Invitation",
+    description:
+      "Join us as we celebrate our special day on December 17, 2026.",
+    images: [
+      "https://aiza-jomari-wedding-rsvp.vercel.app/couple/hero.jpg",
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -25,11 +54,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-  <body className={`${heading.variable} ${body.variable} antialiased`}>
-    <ThemeProvider>
-      {children}
-    </ThemeProvider>
-  </body>
-</html>
+      <body className={`${heading.variable} ${body.variable} antialiased`}>
+        <ThemeProvider>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
